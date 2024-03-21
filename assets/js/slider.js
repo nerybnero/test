cc8a5a67489fa0b5f7effa86eb3d10ed35d7e7be
GRblSHA <dron-skm1234@mail.ru>
2024-03-21 10:30:00 +0300

    Первый коммит

---
+++ assets/js/slider.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const photoSlider = new Swiper('.photo__swiper', {
+        slidesPerView: 4,
+        loop: true,
+        spaceBetween: 30,
+    });
+});
